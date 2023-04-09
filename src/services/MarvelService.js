a3f0fb0182380a145c1b43f2d12dc6f67a723995
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2023-04-10 01:17:36 +0300

    project initialization, marvelservice created

---
+++ src/services/MarvelService.js
@@ -0,0 +1,19 @@
+
+
+class MarvelService {
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=612399cd65672cbcfc4caa418fd9f0a7');
+    }
+}
+
+export default MarvelService;
