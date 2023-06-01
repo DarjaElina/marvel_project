93ac85bc997592d11a7d046980ac91069f27a4c2
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2023-06-01 15:37:34 +0300

    focus in CharList fixed

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,5 @@
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import {useState, useEffect, useRef} from 'react';
+import {useState, useEffect, useRef, useMemo} from 'react';
 import PropTypes from 'prop-types';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -78,6 +78,7 @@ const CharList = (props) => {
 
 
     function renderItems(arr) {
+        console.log('render')
         
         const items =  arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
@@ -121,12 +122,14 @@ const CharList = (props) => {
 
     
     
-    
+    const elements = useMemo(() => {
+        return setContent(process, () => renderItems(charList), newItemLoading);
+    }, [process])
 
 
     return (
         <div className="char__list">
-            {setContent(process, () => renderItems(charList), newItemLoading)}
+            {elements}
             <button 
                 className="button button__main button__long"
                 disabled={newItemLoading}
